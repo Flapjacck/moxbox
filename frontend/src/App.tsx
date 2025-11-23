3943bddce2f01cfa6718552950fa6cf0f5aeb890
Flapjacck <plkelly333@me.com>
2025-11-23 17:17:16 -0500

    Fix: app.tsx uses router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthPage } from "./pages/Auth";
+import { FileDashboard } from "./pages/FileDashboard";
 
 function App() {
-  return <AuthPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/files" element={<FileDashboard />} />
+        <Route path="/" element={<Navigate to="/files" replace />} />
+        <Route path="*" element={<Navigate to="/files" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
