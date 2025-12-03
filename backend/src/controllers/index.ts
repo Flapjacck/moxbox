4ff1773e6b1f7fc3bee88ccbf14b946402295cfc
Flapjacck <plkelly333@me.com>
2025-12-03 11:26:33 -0500

    Feat: move file controllers to controllers/files and update controller exports

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -3,7 +3,5 @@
 // Re-export user controller functions
 export * from './userController';
 
-// Re-export file controller functions
-export * from './fileController';
-// Re-export file admin functions (metadata & lifecycle; contains id-based routes)
-export * from './fileAdminController';
+// Re-export all file controller functions from modular files
+export * from './files';
